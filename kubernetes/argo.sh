50216c90191f2e28d0b2059f6903765fc616c140
Marcelo Garcia Castillo <marcelo.garcia.c92@gmail.com>
2023-12-06 20:35:57 -0300

    ADD: new kubernetes files

---
+++ kubernetes/argo.sh
@@ -0,0 +1,14 @@
+export ARGO_WORKFLOWS_VERSION=v3.5.2
+
+kubectl create namespace argo
+
+kubectl apply -n argo -f https://github.com/argoproj/argo-workflows/releases/download/$ARGO_WORKFLOWS_VERSION/install.yaml
+
+kubectl patch deployment \
+  argo-server \
+  --namespace argo \
+  --type='json' \
+  -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/args", "value": [
+  "server",
+  "--auth-mode=server"
+]}]'
